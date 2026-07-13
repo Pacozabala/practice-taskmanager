2fd3a1ba6b7961ab84cb6389875a86d0231ea234
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-07-13 18:54:16 +0800

    feat: Task getters and setters

--- taskmanager/src/main/java/com/pacozabala/taskmanager/model/Task.java
+++ taskmanager/src/main/java/com/pacozabala/taskmanager/model/Task.java
@@ -20,4 +20,43 @@ public class Task {
     private boolean completed;
     private LocalDateTime createdAt;
     private LocalDate dueDate;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    
 }
