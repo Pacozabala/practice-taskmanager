c4b90d28a19694a12c76db879e212e0a8fa334b4
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-08-05 20:07:55 +0800

    refactor: transferred business logic to service

---
+++ taskmanager/src/main/java/com/pacozabala/taskmanager/service/TaskService.java
@@ -0,0 +1,74 @@
+package com.pacozabala.taskmanager.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.pacozabala.taskmanager.model.Task;
+import com.pacozabala.taskmanager.repository.TaskRepository;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> getTask(Long id) {
+        Optional<Task> taskOptional = taskRepository.findById(id);
+
+        if (taskOptional.isPresent()) {
+            Task foundTask = taskOptional.get();
+            return ResponseEntity.ok(foundTask);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<Task> postTask(Task task) {
+        Task newTask = taskRepository.save(task);
+        
+        return ResponseEntity.ok(newTask);
+    }
+
+    public ResponseEntity<Task> putTask(Long id, Task updatedTask) {
+            
+        Optional<Task> taskOptional = taskRepository.findById(id);
+        
+        if (taskOptional.isPresent()) {
+            Task foundTask = taskOptional.get();
+
+            foundTask.setTitle(updatedTask.getTitle());
+            foundTask.setDescription(updatedTask.getDescription());
+            foundTask.setDueDate(updatedTask.getDueDate());
+            foundTask.setCompleted(updatedTask.isCompleted());
+
+            Task savedTask = taskRepository.save(foundTask);
+
+            return ResponseEntity.ok(savedTask);
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<Void> deleteTask(Long id) {
+
+        Optional<Task> taskOptional = taskRepository.findById(id);
+
+        if (taskOptional.isPresent()) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
