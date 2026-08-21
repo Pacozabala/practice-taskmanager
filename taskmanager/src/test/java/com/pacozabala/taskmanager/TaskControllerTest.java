6c4691946b761c532a5fa5f6532b6a62961a5faf
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-08-21 21:33:22 +0800

    test: completed create, update, delete tests

--- taskmanager/src/test/java/com/pacozabala/taskmanager/TaskControllerTest.java
+++ taskmanager/src/test/java/com/pacozabala/taskmanager/TaskControllerTest.java
@@ -10,12 +10,19 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.pacozabala.taskmanager.dto.TaskResponse;
+
+import tools.jackson.databind.ObjectMapper;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TaskControllerTest {
     
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void createTask() throws Exception {
@@ -34,15 +41,15 @@ public class TaskControllerTest {
             .andExpect(jsonPath("$.title").value("Test task"));
     }
 
-    @Test
-    void getTasks() throws Exception {
-        mockMvc.perform(get("/tasks"))
-            .andExpect(status().isOk());
-    }
+    // @Test
+    // void getTasks() throws Exception {
+    //     mockMvc.perform(get("/tasks"))
+    //         .andExpect(status().isOk());
+    // }
 
     @Test
     void updateTask() throws Exception {
-        String createRequest = """
+        String request = """
             {
                 "title": "Original task",
                 "description": "Original description",
@@ -52,13 +59,16 @@ public class TaskControllerTest {
 
         String response = mockMvc.perform(post("/tasks")
             .contentType(MediaType.APPLICATION_JSON)
-            .content(createRequest))
+            .content(request))
             .andExpect(status().isOk())
             .andReturn()
             .getResponse()
             .getContentAsString();
 
         // extract ID from response
+        TaskResponse createdTask = objectMapper.readValue(response, TaskResponse.class);
+        
+        Long id = createdTask.getId();
 
         String updateRequest = """
         {
@@ -69,17 +79,37 @@ public class TaskControllerTest {
         """;
 
         // PUT /tasks/{id}
+        mockMvc.perform(put("/tasks/{id}", id)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(updateRequest))
+            .andExpect(status().isOk());
     }
 
     @Test
     void deleteTask() throws Exception {
-        String createRequest = """
+        String request = """
             {
                 "title": "Original task",
                 "description": "Original description",
                 "dueDate": "2026-08-30"
             }
             """;
+
+        String response = mockMvc.perform(post("/tasks")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(request))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        // extract ID from response
+        TaskResponse createdTask = objectMapper.readValue(response, TaskResponse.class);
+        
+        Long id = createdTask.getId();
+
+        mockMvc.perform(delete("/tasks/{id}", id))
+        .andExpect(status().isOk());
     }
 
     @Test
